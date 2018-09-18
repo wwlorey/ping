f6db66a03be9a8fa67059a432cc222025446f512
William Lorey <wwlorey@gmail.com>
2018-09-18 13:59:09 -0500

    Add optional excercise 1

--- UDP_Pinger_Client.py
+++ UDP_Pinger_Client.py
@@ -12,17 +12,20 @@ UDP_PORT = 12000
 NUM_PINGS = 10
 BUFF_SIZE = 1024
 
+# Initialize variables
+rtt_list = [None] * NUM_PINGS
+
 # Create a UDP socket
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
 # Set a 1sec timeout for the socket
 clientSocket.settimeout(TIMEOUT)
 
-for i in range(1, NUM_PINGS + 1):
+for ping_num in range(1, NUM_PINGS + 1):
     oldNow = datetime.datetime.now()
 
     # Create a ping message
-    pingMessage = 'Ping ' + str(i) + ' ' + str(oldNow.time())
+    pingMessage = 'Ping ' + str(ping_num) + ' ' + str(oldNow.time())
 
     # Send a message
     clientSocket.sendto(pingMessage.encode(), (UDP_IP, UDP_PORT))
@@ -38,7 +41,26 @@ for i in range(1, NUM_PINGS + 1):
         # Print the response message and RTT
         print(message.decode())
         print(rtt)
+
+        # Save off the RTT
+        rtt_list[ping_num - 1] = rtt
+        
     except:
         print('Response timed out')
 
     print()
+
+# Calculate and print RTT stats
+num_dropped_packets = rtt_list.count(None)
+rtt_list = [f for f in rtt_list if f]
+
+min_rtt = min(rtt_list)
+max_rtt = max(rtt_list)
+avg_rtt = sum(rtt_list) / len(rtt_list)
+packet_loss = (num_dropped_packets / NUM_PINGS) * 100
+
+print('RTT Statistics')
+print('min RTT:\t' + str(min_rtt))
+print('max RTT:\t' + str(max_rtt))
+print('average RTT:\t' + str(avg_rtt))
+print('packet loss:\t' + str(packet_loss) + '%')
